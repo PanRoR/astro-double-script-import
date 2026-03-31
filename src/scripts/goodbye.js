055a691f0969dbab77a14bf2cf3717e3aeafc634
PanRoR <calvin.hanisch@gmail.com>
2026-03-31 17:08:35 +0200

    add: some files to import/export. add: astro/netlify adapter.

---
+++ src/scripts/goodbye.js
@@ -0,0 +1,3 @@
+console.log("from goodbye.js");
+
+export const greeting = "goodbye";
---
+++ src/scripts/hello.js
@@ -0,0 +1,8 @@
+import { greeting } from "./goodbye";
+
+console.log("hello");
+console.log(greeting);
+
+const span = document.querySelector("span");
+
+span.style.color = "red";
